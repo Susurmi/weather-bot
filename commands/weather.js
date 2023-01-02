828c8c5cc2cadcdcb8f32919b41a13432e8a3bf7
Alexander Surmann <80062043+Susurmi@users.noreply.github.com>
2023-01-02 09:36:29 +0100

    Some light formatting changes
    Did some embed changes.

--- commands/weather.js
+++ commands/weather.js
@@ -15,17 +15,17 @@ module.exports = {
     ),
   async execute(interaction) {
     function degToText(degree) {
-      if (degree > 337.5) return 'Northerly';
-      if (degree > 292.5) return 'North Westerly';
-      if (degree > 247.5) return 'Westerly';
-      if (degree > 202.5) return 'South Westerly';
-      if (degree > 157.5) return 'Southerly';
-      if (degree > 122.5) return 'South Easterly';
-      if (degree > 67.5) return 'Easterly';
+      if (degree > 337.5) return 'Northerly ⬆';
+      if (degree > 292.5) return 'North Westerly ↖';
+      if (degree > 247.5) return 'Westerly ⬅';
+      if (degree > 202.5) return 'South Westerly ↙';
+      if (degree > 157.5) return 'Southerly ⬇';
+      if (degree > 122.5) return 'South Easterly ↘';
+      if (degree > 67.5) return 'Easterly ➡';
       if (degree > 22.5) {
-        return 'North Easterly';
+        return 'North Easterly ↗';
       }
-      return 'Northerly';
+      return 'Northerly ⬆';
     }
     const deUmlaut = (word) => {
       word = word.replace(/ä/g, 'ae');
@@ -39,51 +39,62 @@ module.exports = {
     axios({
       method: 'get',
       url: apiUrl,
-    }).then(async (response) => {
-      const embed = new EmbedBuilder()
-        .setTitle(`Weather in ${response.data.name}`)
-        .setColor('Default')
-        .setThumbnail(
-          `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-        )
-        .addFields(
-          {
-            name: 'Degrees',
-            value: `${Math.floor(response.data.main.temp)}°C`,
-            inline: true,
-          },
-          {
-            name: 'Weather',
-            value: `${response.data.weather[0].main}`,
-            inline: true,
-          },
-          {
-            name: 'Humidity',
-            value: `${response.data.main.humidity}%`,
-            inline: true,
-          }
-        )
-        .addFields(
-          {
-            name: 'Wind Speed',
-            value: `${response.data.wind.speed} meter/sec`,
-            inline: true,
-          },
-          {
-            name: 'Wind Direction',
-            value: `${degToText(response.data.wind.deg)}`,
-            inline: true,
-          },
-          {
-            name: 'Measured',
-            value: `${moment
-              .unix(response.data.dt)
-              .utcOffset(+1)
-              .format('dddd DD-MMMM-YYYY HH:mm')} \n (UTC+1 Berlin)`,
-            inline: true,
-          }
-        );
-      await interaction.reply({ embeds: [embed] });
-    });
+    })
+      .then(async (response) => {
+        const embed = new EmbedBuilder()
+          .setTitle(`Weather in **${response.data.name}**`)
+          .setColor('Default')
+          .setThumbnail(
+            `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+          )
+          .addFields(
+            {
+              name: 'Degrees',
+              value: `${Math.floor(response.data.main.temp)}°C`,
+              inline: true,
+            },
+            {
+              name: 'Weather',
+              value: `${response.data.weather[0].main}`,
+              inline: true,
+            },
+            {
+              name: 'Humidity',
+              value: `${response.data.main.humidity}%`,
+              inline: true,
+            }
+          )
+          .addFields(
+            {
+              name: 'Wind Speed',
+              value: `${response.data.wind.speed} meter/sec`,
+              inline: true,
+            },
+            {
+              name: 'Wind Direction',
+              value: `${degToText(response.data.wind.deg)}`,
+              inline: true,
+            },
+            {
+              name: 'Measured',
+              value: `${moment
+                .unix(response.data.dt)
+                .utcOffset(+1)
+                .format('DD-MM-YYYY HH:mm')} \n (UTC+1 Berlin)`,
+              inline: true,
+            }
+          );
+        await interaction.reply({ embeds: [embed] });
+        return;
+      })
+      .catch(async (error) => {
+        console.log(error);
+        await interaction.reply({
+          content:
+            'There was an error while executing this command! Please try again later.',
+          ephemeral: true,
+        });
+        return;
+      });
   },
 };
