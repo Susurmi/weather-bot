a509e7c3733f3262b76b243c04b2a1f53d133670
Alexander <80062043+Susurmi@users.noreply.github.com>
2023-08-30 19:34:33 +0200

    Project Update
    added nodemon & eslint as dev dependencies.
    added elsintrc.
    updated code to match linter settings.
    updated all dependencies.

--- commands/weather.js
+++ commands/weather.js
@@ -1,97 +1,103 @@
 const axios = require('axios');
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, CommandInteraction } = require('discord.js');
 const moment = require('moment');
 moment.locale('en');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('weather')
-    .setDescription('shows the weather for a location.')
-    .addStringOption((option) =>
-      option
-        .setName('city')
-        .setDescription('which city do you want to know the weather of?')
-        .setRequired(true)
-    ),
-  async execute(interaction) {
-    function degToText(degree) {
-      if (degree > 337.5) return 'Northerly ⬆';
-      if (degree > 292.5) return 'North Westerly ↖';
-      if (degree > 247.5) return 'Westerly ⬅';
-      if (degree > 202.5) return 'South Westerly ↙';
-      if (degree > 157.5) return 'Southerly ⬇';
-      if (degree > 122.5) return 'South Easterly ↘';
-      if (degree > 67.5) return 'Easterly ➡';
-      if (degree > 22.5) {
-        return 'North Easterly ↗';
-      }
-      return 'Northerly ⬆';
-    }
-    const deUmlaut = (word) => {
-      word = word.replace(/ä/g, 'ae');
-      word = word.replace(/ö/g, 'oe');
-      word = word.replace(/ü/g, 'ue');
-      word = word.replace(/ß/g, 'ss');
-      return word;
-    };
-    const city = deUmlaut(interaction.options.getString('city'));
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.WEATHER_API_KEY}`;
-    axios({
-      method: 'get',
-      url: apiUrl,
-    })
-      .then(async (response) => {
-        const embed = new EmbedBuilder()
-          .setTitle(`Weather in **${response.data.name}**`)
-          .setColor('Default')
-          .setThumbnail(`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
-          .addFields(
-            {
-              name: 'Degrees',
-              value: `${Math.floor(response.data.main.temp)}°C`,
-              inline: true,
-            },
-            {
-              name: 'Weather',
-              value: `${response.data.weather[0].main}`,
-              inline: true,
-            },
-            {
-              name: 'Humidity',
-              value: `${response.data.main.humidity}%`,
-              inline: true,
-            }
-          )
-          .addFields(
-            {
-              name: 'Wind Speed',
-              value: `${response.data.wind.speed} meter/sec`,
-              inline: true,
-            },
-            {
-              name: 'Wind Direction',
-              value: `${degToText(response.data.wind.deg)}`,
-              inline: true,
-            },
-            {
-              name: 'Measured',
-              value: `${moment
-                .unix(response.data.dt)
-                .utcOffset(+1)
-                .format('DD-MM-YYYY HH:mm')} \n (UTC+1 Berlin)`,
-              inline: true,
-            }
-          );
-        await interaction.reply({ embeds: [embed] });
-        return;
-      })
-      .catch(async (error) => {
-        console.log(error);
-        await interaction.reply({
-          content: 'There was an error while executing this command! Please try again later.',
-          ephemeral: true,
-        });
-        return;
-      });
-  },
+	data: new SlashCommandBuilder()
+		.setName('weather')
+		.setDescription('shows the weather for a location.')
+		.addStringOption((option) =>
+			option
+				.setName('city')
+				.setDescription('which city do you want to know the weather of?')
+				.setRequired(true),
+		),
+
+	/**
+	 * @param {CommandInteraction} interaction
+	 */
+
+	async execute(interaction) {
+
+		function degToText(degree) {
+			if (degree > 337.5) return 'Northerly ⬆';
+			if (degree > 292.5) return 'North Westerly ↖';
+			if (degree > 247.5) return 'Westerly ⬅';
+			if (degree > 202.5) return 'South Westerly ↙';
+			if (degree > 157.5) return 'Southerly ⬇';
+			if (degree > 122.5) return 'South Easterly ↘';
+			if (degree > 67.5) return 'Easterly ➡';
+			if (degree > 22.5) {
+				return 'North Easterly ↗';
+			}
+			return 'Northerly ⬆';
+		}
+		const deUmlaut = (word) => {
+			word = word.replace(/ä/g, 'ae');
+			word = word.replace(/ö/g, 'oe');
+			word = word.replace(/ü/g, 'ue');
+			word = word.replace(/ß/g, 'ss');
+			return word;
+		};
+		const city = deUmlaut(interaction.options.getString('city'));
+		const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.WEATHER_API_KEY}`;
+		axios({
+			method: 'get',
+			url: apiUrl,
+		})
+			.then(async (response) => {
+				const embed = new EmbedBuilder()
+					.setTitle(`Weather in **${response.data.name}**`)
+					.setColor('Default')
+					.setThumbnail(`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+					.addFields(
+						{
+							name: 'Degrees',
+							value: `${Math.floor(response.data.main.temp)}°C`,
+							inline: true,
+						},
+						{
+							name: 'Weather',
+							value: `${response.data.weather[0].main}`,
+							inline: true,
+						},
+						{
+							name: 'Humidity',
+							value: `${response.data.main.humidity}%`,
+							inline: true,
+						},
+					)
+					.addFields(
+						{
+							name: 'Wind Speed',
+							value: `${response.data.wind.speed} meter/sec`,
+							inline: true,
+						},
+						{
+							name: 'Wind Direction',
+							value: `${degToText(response.data.wind.deg)}`,
+							inline: true,
+						},
+						{
+							name: 'Measured',
+							value: `${moment
+								.unix(response.data.dt)
+								.utcOffset(+1)
+								.format('DD-MM-YYYY HH:mm')} \n (UTC+1 Berlin)`,
+							inline: true,
+						},
+					);
+				await interaction.reply({ embeds: [embed] });
+				return;
+			})
+			.catch(async (error) => {
+				console.log(error);
+				await interaction.reply({
+					content: 'There was an error while executing this command! Please try again later.',
+					ephemeral: true,
+				});
+				return;
+			});
+	},
 };
--- index.js
+++ index.js
@@ -11,40 +11,40 @@ const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-  if ('data' in command && 'execute' in command) {
-    bot.commands.set(command.data.name, command);
-  } else {
-    console.log(
-      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-    );
-  }
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	if ('data' in command && 'execute' in command) {
+		bot.commands.set(command.data.name, command);
+	} else {
+		console.log(
+			`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
+		);
+	}
 }
 
 bot.once(Events.ClientReady, (client) => {
-  console.log(`Ready! Logged in as ${client.user.tag}`);
+	console.log(`Ready! Logged in as ${client.user.tag}`);
 });
 
 bot.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
+	if (!interaction.isChatInputCommand()) return;
 
-  const command = interaction.client.commands.get(interaction.commandName);
+	const command = interaction.client.commands.get(interaction.commandName);
 
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
-  }
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
 
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({
-      content: 'There was an error while executing this command!',
-      ephemeral: true,
-    });
-  }
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({
+			content: 'There was an error while executing this command!',
+			ephemeral: true,
+		});
+	}
 });
 
 deployCommands().then(() => bot.login(process.env.BOT_TOKEN));
